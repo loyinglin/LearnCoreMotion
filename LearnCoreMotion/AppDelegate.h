9d83f8998b009aa06756292f26015626eb6906ec
loyinglin <loying@foxmail.com>
2019-01-10 17:49:25 +0800

    LearnCoreMotion

---
+++ LearnCoreMotion/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LearnCoreMotion
+//
+//  Created by loyinglin on 2019/1/10.
+//  Copyright © 2019 ByteDance. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LearnCoreMotion/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  LearnCoreMotion
+//
+//  Created by loyinglin on 2019/1/10.
+//  Copyright © 2019 ByteDance. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
